b995796c9d9d9f4adbb8da629d517ef39abae5b3
Joern Engel <joern@purestorage.com>
2023-05-11 15:38:15 -0700

    512bit wide crc and minor optimizations
    On Sapphire Rapids the wide version gets close to 32B/c.
    
    Signed-off-by: Joern Engel <joern@purestorage.com>

--- crc64.c
+++ crc64.c
@@ -74,8 +74,7 @@ u64 crc64_clmul(u64 crc, const void *data, size_t n)
 		b = fold2(b, mu4, f);
 		c = fold2(c, mu4, g);
 		d = fold2(d, mu4, h);
-chain4:
-		while (n>=64) {
+		if (n>=64) {
 			a = fold3(a, mu4, data+ 0);
 			b = fold3(b, mu4, data+16);
 			c = fold3(c, mu4, data+32);
@@ -83,16 +82,17 @@ chain4:
 			n -= 64;
 			data += 64;
 		}
+chain4:
 		/* We reduce 4 chains down to 2. */
 		a = fold2(a, mu2, c);
 		b = fold2(b, mu2, d);
-chain2:
-		while (n>=32) {
+		if (n>=32) {
 			a = fold3(a, mu2, data+ 0);
 			b = fold3(b, mu2, data+16);
 			n -= 32;
 			data += 32;
 		}
+chain2:
 		/* We reduce 2 chains down to 1. */
 		a = fold2(a, mu1, b);
 		if (n>24) {
@@ -148,3 +148,118 @@ chain2:
 	d = c ^ a;				/* xor with low bits of a */
 	return ~_mm_extract_epi64(d, 1);
 }
+
+
+static inline __m512i xor3(__m512i a, __m512i b, __m512i c)
+{
+	return _mm512_ternarylogic_epi64(a, b, c, 0x96);
+}
+
+static inline __m512i pfold1(__m512i acc, __m512i mu)
+{
+	return _mm512_clmulepi64_epi128(acc, mu, 0x00) ^ _mm512_clmulepi64_epi128(acc, mu, 0x11);
+}
+
+static inline __m512i pfold2(__m512i acc, __m512i mu, __m512i src)
+{
+	return xor3(src, _mm512_clmulepi64_epi128(acc, mu, 0x00), _mm512_clmulepi64_epi128(acc, mu, 0x11));
+}
+
+static inline __m512i pfold3(__m512i acc, __m512i mu, const void *src)
+{
+	return pfold2(acc, mu, _mm512_loadu_si512(src));
+}
+
+static inline __m512i mu512(u64 a, u64 b)
+{
+	return _mm512_set_epi64(a, b, a, b, a, b, a, b);
+}
+
+u64 crc64_pclmul(u64 crc, const void *data, size_t n)
+{
+	__m512i mu4 = mu512(0xf31fd9271e228b79ull, 0x8260adf2381ad81cull); /* 1<<1984 % poly, 1<<2048 % poly */
+	__m512i mu2 = mu512(0xd7d86b2af73de740ull, 0x8757d71d4fcc1000ull); /* 1<< 960 % poly, 1<<1024 % poly */
+	__m512i mu1 = mu512(0x081f6054a7842df4ull, 0x6ae3efbb9dd441f3ull); /* 1<< 448 % poly, 1<< 512 % poly */
+	__m512i a, b, c, d;
+	void *start;
+
+	if (n>=256) {
+		/*
+		 * When possible, we start using 4 fold chains, consuming 256
+		 * bytes or 2048 bits at a time.
+		 */
+		if ((unsigned long)data&63) {
+			/* Align data first - unaligned 512b-reads are expensive */
+			u8 buf[192] = {};
+			size_t skip = 64-((unsigned long)data&63);
+			memcpy(buf+skip, data, 128);
+			write64(buf+skip, ~crc ^ read64(buf+skip));
+
+			a = _mm512_loadu_si512(buf);
+			b = _mm512_loadu_si512(buf+64);
+			n -= 128-skip;
+			data += 128-skip;
+			c = _mm512_loadu_si512(data+0);
+			d = _mm512_loadu_si512(data+64);
+			n -= 128;
+			data += 128;
+		} else {
+			a = _mm512_loadu_si512(data) ^ _mm512_set_epi64(0, 0, 0, 0, 0, 0, 0, ~crc);
+			b = _mm512_loadu_si512(data+ 64);
+			c = _mm512_loadu_si512(data+128);
+			d = _mm512_loadu_si512(data+192);
+			n -= 256;
+			data += 256;
+		}
+		while (n>=256) {
+			a = pfold3(a, mu4, data+  0);
+			b = pfold3(b, mu4, data+ 64);
+			c = pfold3(c, mu4, data+128);
+			d = pfold3(d, mu4, data+192);
+			n -= 256;
+			data += 256;
+		}
+		/* We reduce 4 chains down to 2. */
+		a = pfold2(a, mu2, c);
+		b = pfold2(b, mu2, d);
+		if (n>=128) {
+			a = pfold3(a, mu2, data+ 0);
+			b = pfold3(b, mu2, data+64);
+			n -= 128;
+			data += 128;
+		}
+chain2:
+		/* We reduce 2 chains down to 1. */
+		a = pfold2(a, mu1, b);
+		if (n>=64) {
+			a = pfold3(a, mu1, data);
+			n -= 64;
+			data += 64;
+		}
+		/*
+		 * And finally we copy our accumulator a and the remaining data
+		 * to a 128B buffer.  Reducing 128B to the final crc happens
+		 * uses the narrow clmul instruction:
+		 * | 0-padding  | a        | data       |
+		 * | 0-64 bytes | 64 bytes | 0-64 bytes |
+		 */
+		u8 buf[128] = {};
+		start = buf+sizeof(buf)-n;
+		_mm512_storeu_si512(start-64, a);
+		memcpy(start, data, n);
+		return crc64_clmul(~0ull, buf, sizeof(buf));
+	} else if (n>128) {
+		/* Set up 2 accumulators before jumping to common code */
+		a = _mm512_loadu_si512(data) ^ _mm512_set_epi64(0, 0, 0, 0, 0, 0, 0, ~crc);
+		b = _mm512_loadu_si512(data+64);
+		n -= 128;
+		data += 128;
+		goto chain2;
+	} else {
+		return crc64_clmul(crc, data, n);
+	}
+}
+
+/*
+ * Function dispatcher and generic version are left as an exercise to the reader.
+ */
